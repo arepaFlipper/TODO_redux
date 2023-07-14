30c803743b063ea70bf75809dfb36cae16c6312e
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-14 18:54:58 -0500

    🌌 patch

--- src/features/posts/ReactionButtons.jsx
+++ src/features/posts/ReactionButtons.jsx
@@ -11,12 +11,8 @@ const reactionEmoji = {
 
 export const ReactionButtons = ({ post }) => {
   const [addReaction] = useAddReactionMutation();
-  console.log(`📯%cReactionButtons.jsx:16 - post.reactions`, 'font-weight:bold; background:#4cb300;color:#fff;'); //DELETEME
-  console.log(post.reactions); // DELETEME
   const ReactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     const count_reactions = post.reactions[name];
-    console.log(`🍕%cReactionButtons.jsx:18 - count_reactions`, 'font-weight:bold; background:#53ac00;color:#fff;'); //DELETEME
-    console.log(count_reactions); // DELETEME
     return (
       <button
         key={name}
